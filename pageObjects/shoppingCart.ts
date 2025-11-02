3405d392231026618b9e319cb2d99ac99fa5bf37
Alexander Lazaris <alex.lazaris617@gmail.com>
2025-11-02 21:51:25 +1100

    feat: more page objects added

---
+++ pageObjects/shoppingCart.ts
@@ -0,0 +1,21 @@
+import { Locator, Page } from "playwright/test";
+
+export class ShoppingCartPage {
+    readonly page: Page;
+    readonly shoppingCarLink: Locator;
+    readonly shoppingCarBadgeNumber: Locator;
+    readonly menuButton: Locator;
+    readonly logoutButton: Locator;
+
+    constructor(page: Page) {
+        this.page = page;
+        this.shoppingCarLink= page.locator('.shopping_cart_link');
+        this.shoppingCarBadgeNumber= page.locator('.shopping_cart_badge');
+        this.menuButton= page.locator('#react-burger-menu-btn');
+        this.logoutButton= page.locator('#logout_sidebar_link');
+    }
+
+    
+
+}
+
