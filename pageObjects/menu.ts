3c6263379eee251eba460265697dcfa7702a3675
alaz-linux <alex.lazaris617@gmail.com>
2025-11-02 23:43:14 +1100

    style: formatting changes

--- pageObjects/menu.ts
+++ pageObjects/menu.ts
@@ -3,19 +3,21 @@ import { Locator, Page } from "playwright/test";
 export class MenuBar {
     readonly page: Page;
     readonly shoppingCarLink: Locator;
-    readonly shoppingCarBadgeNumber: Locator;
+    readonly shoppingCartBadgeNumber: Locator;
     readonly menuButton: Locator;
     readonly logoutButton: Locator;
 
     constructor(page: Page) {
         this.page = page;
-        this.shoppingCarLink= page.locator('.shopping_cart_link');
-        this.shoppingCarBadgeNumber= page.locator('.shopping_cart_badge');
-        this.menuButton= page.locator('#react-burger-menu-btn');
-        this.logoutButton= page.locator('#logout_sidebar_link');
+        this.shoppingCarLink = page.locator('.shopping_cart_link');
+        this.shoppingCartBadgeNumber = page.locator('.shopping_cart_badge');
+        this.menuButton = page.locator('button#react-burger-menu-btn');
+        this.logoutButton = page.locator('#logout_sidebar_link');
     }
 
-
-
+    async logoutOfApp() {
+        await this.menuButton.click();
+        await this.logoutButton.click();
+    }
 }
 
