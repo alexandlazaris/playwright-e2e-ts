66e9f70f52f90b7a07838ef34871c5504ae0e403
Alexander Lazaris <alex.lazaris617@gmail.com>
2025-12-03 21:58:22 +1100

    ci: use different playwright config setting when in ci + temp include feature branch on push

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,7 +22,9 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: process.env.CI
+    ? [['github'], ['html', { outputFolder: 'playwright-report', open: 'never' }]]
+    : 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
@@ -33,7 +35,7 @@ export default defineConfig({
     trace: 'on-first-retry',
 
     launchOptions: {
-      slowMo: 100
+      slowMo: process.env.CI ? 0 : 100
     },
   },
 
